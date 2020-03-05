5c0f2d175d6514f7eb8d569a54fad29223090045
Daniel Hladek <daniel.hladek@tuke.sk>
2020-03-05 11:58:05 +0100

    zz

--- train.sh
+++ train.sh
@@ -1,1 +1,5 @@
+mkdir build/train
+mkdir build/train/output
+mkdir dist
 spacy train sk ./build/train/output ./build/input/slovak-treebank ./build/input/ud-artificial-gapping --pipeline tagger,parser --n-iter 10
+spacy package build/train/output/model-best dist
