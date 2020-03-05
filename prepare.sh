6a01efa29a67e4ee678b05cecf58fcd5d22866d2
Daniel Hladek <dhladek@gmail.com>
2020-03-05 06:31:35 +0100

    zz

---
+++ prepare.sh
@@ -0,0 +1,2 @@
+#spacy convert ./sources/slovak-treebank/stb.conll ./build/input/slovak-treebank
+spacy convert ./sources/ud-artificial-gapping/sk-ud-crawled-orphan.conllu ./build/input/ud-artificial-gapping
---
+++ train.sh
@@ -0,0 +1,1 @@
+spacy train sk ./build/train/output ./build/input/slovak-treebank ./build/input/ud-artificial-gapping --pipeline tagger,parser --n-iter 10
