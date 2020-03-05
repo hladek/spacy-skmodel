99ef661603e47f7c235cde209ec316214224d171
Daniel Hladek <daniel.hladek@tuke.sk>
2020-03-05 15:25:27 +0100

    fix meta in package

--- train.sh
+++ train.sh
@@ -2,4 +2,6 @@ mkdir build/train
 mkdir build/train/output
 mkdir dist
 spacy train sk ./build/train/output ./build/input/slovak-treebank ./build/input/ud-artificial-gapping --pipeline tagger,parser --n-iter 10
-spacy package build/train/output/model-best dist
+spacy package build/train/output/model-best dist --meta-path ./meta.json --force
+cd dist/sk_sk1-0.0.1
+python ./setup.py sdist --dist-dir ../
