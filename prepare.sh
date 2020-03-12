924964affd58643bd7aa1f4c16c0b1c1ac55ed95
Daniel Hladek <dhladek@gmail.com>
2020-03-12 10:31:06 +0100

    zz

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,10 @@
-mkdir build
-mkdir build/input
-mkdir build/input/slovak-treebank
-mkdir build/input/ud-artificial-gapping
-spacy convert ./sources/slovak-treebank/stb.conll ./build/input/slovak-treebank
-spacy convert ./sources/ud-artificial-gapping/sk-ud-crawled-orphan.conllu ./build/input/ud-artificial-gapping
+
+#mkdir build
+#mkdir build/input
+#mkdir build/input/slovak-treebank
+#spacy convert ./sources/slovak-treebank/stb.conll ./build/input/slovak-treebank
+#mkdir build/input/ud-artificial-gapping
+#spacy convert ./sources/ud-artificial-gapping/sk-ud-crawled-orphan.conllu ./build/input/ud-artificial-gapping
+#mkdir build/skner
+cat ./sources/skner/wikiann-sk.bio | python ./sources/bio-to-iob.py > build/skner/wikiann-sk.iob
+spacy convert -n 10 --converter ner ./build/skner/wikiann-sk.iob ./build/slovak-treebank
