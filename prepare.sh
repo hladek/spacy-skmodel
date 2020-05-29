e26ba79260cc3e14498a4e6716ea4b065dba0062
Daniel Hladek <dhladek@gmail.com>
2020-05-29 06:50:31 +0200

    zz

--- prepare.sh
+++ prepare.sh
@@ -1,10 +1,19 @@
-
-#mkdir build
-#mkdir build/input
-#mkdir build/input/slovak-treebank
-#spacy convert ./sources/slovak-treebank/stb.conll ./build/input/slovak-treebank
-#mkdir build/input/ud-artificial-gapping
-#spacy convert ./sources/ud-artificial-gapping/sk-ud-crawled-orphan.conllu ./build/input/ud-artificial-gapping
-#mkdir build/skner
-cat ./sources/skner/wikiann-sk.bio | python ./sources/bio-to-iob.py > build/skner/wikiann-sk.iob
-spacy convert -n 10 --converter ner ./build/skner/wikiann-sk.iob ./build/slovak-treebank
+mkdir -p build
+mkdir -p build/input
+# Prepare Treebank
+mkdir -p build/input/slovak-treebank
+spacy convert ./sources/slovak-treebank/stb.conll ./build/input/slovak-treebank
+# UDAG used as evaluation
+mkdir -p build/input/ud-artificial-gapping
+spacy convert ./sources/ud-artificial-gapping/sk-ud-crawled-orphan.conllu ./build/input/ud-artificial-gapping
+# Prepare skner
+mkdir -p build/input/skner
+# Convert to IOB
+cat ./sources/skner/wikiann-sk.bio | python ./sources/bio-to-iob.py > build/input/skner/wikiann-sk.iob
+# Split to train test
+cat ./build/input/skner/wikiann-sk.iob | python ./sources/iob-to-traintest.py ./build/input/skner/wikiann-sk
+# Convert train and test
+mkdir -p build/input/skner-train
+spacy convert -n 15 --converter ner ./build/input/skner/wikiann-sk.train ./build/input/skner
+mkdir -p build/input/skner-test
+spacy convert -n 15 --converter ner ./build/input/skner/wikiann-sk.test ./build/input/skner-test
--- train.sh
+++ train.sh
@@ -1,7 +1,13 @@
-mkdir build/train
-mkdir build/train/output
-mkdir dist
-spacy train sk ./build/train/output ./build/input/slovak-treebank ./build/input/ud-artificial-gapping --pipeline tagger,parser,ner --n-iter 10
-spacy package build/train/output/model-best dist --meta-path ./meta.json --force
+set -e
+OUTDIR=build/train/output
+mkdir -p build/train
+mkdir -p $OUTDIR
+mkdir -p dist
+# Delete old training results
+rm -r $OUTDIR/*
+# Train dependency and POS
+spacy train sk $OUTDIR ./build/input/slovak-treebank ./build/input/ud-artificial-gapping --pipeline tagger,parser --n-iter 10
+# Package model
+spacy package $OUTDIR/model-best dist --meta-path ./meta.json --force
 cd dist/sk_sk1-0.0.1
 python ./setup.py sdist --dist-dir ../
