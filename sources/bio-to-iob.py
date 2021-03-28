e5bb3e483e2c9465454749a38bfede94b447ce5e
Daniel Hladek <hladek@quadro.kemt.fei.tuke.sk>
2021-03-28 17:34:18 +0200

    zz

--- sources/bio-to-iob.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-for l in sys.stdin:
-    line = l.rstrip()
-    tokens = line.split()
-    if len(tokens) < 1:
-        print("")
-        continue
-    print(tokens[0] + " " + tokens[-1])
--- train.sh
+++ train.sh
@@ -1,23 +1,26 @@
 set -e
-OUTDIR=build/train/output
-TRAINDIR=build/train
-mkdir -p $TRAINDIR 
-mkdir -p $OUTDIR
+mkdir -p input
+mkdir -p output
 mkdir -p dist
+FLAGS=-g 1 -V 0.3.0
+
 # Delete old training results
-rm -rf $OUTDIR/*
+mkdir -p posparser
 # Train dependency and POS
-spacy train sk $OUTDIR ./build/input/slovak-treebank ./build/input/ud-artificial-gapping  --n-iter 20 -p tagger,parser
-rm -rf $TRAINDIR/posparser
-# Move trained model
-mv $OUTDIR/model-best $TRAINDIR/posparser
+spacy train sk posparser input/slovak-treebank input/ud-artificial-gapping  --n-iter 10 -p tagger,parser $FLAGS
+mkdir -p nerposparser
 # Train NER
-# -m posparser loads previous model, but does not copy model
-python ./train.py -t ./sources/skner/wikiann-sk.bio -o $TRAINDIR/ner -n 20 -m $TRAINDIR/posparser/
-# Copy previous models to the new model
-cp -r $TRAINDIR/posparser $TRAINDIR/nerposparser/
-cp -r $TRAINDIR/ner/ner $TRAINDIR/nerposparser/ner
+spacy train sk nerposparser input/skner/train.json input/skner/test.json -p ner -b posparser/model-final --n-iter 10 $FLAGS
 # Package model
-spacy package $TRAINDIR/nerposparser dist --meta-path ./meta.json --force
+rm -rf dist
+mkdir -p dist
+spacy package nerposparser/model-final dist
 cd dist/sk_sk1-0.3.0
 python ./setup.py sdist --dist-dir ../
+
+#rm -rf posparser
+#rm -rf nerposparser
+#rm -rf $TRAINDIR/posparser
+# Move trained model
+#mv $OUTDIR/model-best $TRAINDIR/posparser
+#rm -r $OUTDIR/*
