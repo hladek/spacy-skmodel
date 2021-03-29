f0602c0009bf8ccdb62fed14fb9d7a50ce64787e
Daniel Hladek <hladek@quadro.kemt.fei.tuke.sk>
2021-03-29 15:54:57 +0200

    works

--- train.sh
+++ train.sh
@@ -1,25 +1,25 @@
 set -e
 mkdir -p input
-mkdir -p traindir
-rm -rf ./traindir/*
 mkdir -p dist
-FLAGS="-g 1"
+FLAGS="-g 1 --n-iter 2 "
 
 # Delete old training results
-mkdir -p posparser
+mkdir -p traindir
+rm -rf ./traindir/*
 # Train dependency and POS
-spacy train sk traindir input/slovak-treebank input/ud-artificial-gapping  --n-iter 10 -p tagger,parser -m "./meta.json" $FLAGS
+spacy train sk traindir input/slovak-treebank input/ud-artificial-gapping  -p tagger,parser -m "./meta.json" $FLAGS
 rm -rf ./posparser
 mv traindir/model-final ./posparser
 rm -rf ./traindir/*
 # Train NER
-spacy train sk traindir input/skner/train.json input/skner/test.json -p ner -b posparser --n-iter 10 $FLAGS
-mv traindir/model-final ./nerposparser
+spacy train sk traindir input/skner/train.json input/skner/test.json -p ner  -R -b posparser $FLAGS -m posparser/meta.json
 rm -rf nerposparser
+mv traindir/model-final ./nerposparser
 # Package model
 mkdir -p dist
 rm -rf ./dist/*
 spacy package nerposparser dist
-cd dist/sk_sk1-0.3.0
+DNAME=`ls dist`
+cd dist/$DNAME
 python ./setup.py sdist --dist-dir ../
 
