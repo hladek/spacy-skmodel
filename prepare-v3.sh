17d9b896488cfca7e0a1dcfcbce38684095c6db9
Daniel Hladek <hladek@quadro.kemt.fei.tuke.sk>
2021-05-10 15:17:41 +0200

    zz

--- prepare-v3.sh
+++ prepare-v3.sh
@@ -2,3 +2,7 @@ conda install -c conda-forge -c pytorch spacy cupy pytorch cudatoolkit=9.2
 # packages only available via pip
 pip install spacy-transformers spacy-lookups-data
 spacy init config config.cfg -l sk -p tagger,parser
+mkdir -p input
+cat ./sources/slovak-treebank/stb.conll | python ./treebank2json.py input/train.json input/test.json
+spacy convert input/train.json input
+spacy convert input/test.json input
--- treebank2json.py
+++ treebank2json.py
@@ -4,37 +4,82 @@ import json
 #from spacy.gold import offsets_from_biluo_tags
 #from spacy.gold import iob_to_biluo
 
-dataset = []
 
 def save_data(filename,dataset):
-    pass
-
-
-def process_data(filename):
-    with open(filename) as f:
-        sentence = []
-        for l in f:
-            tokens = l.split()
-            head = int(tokens[5])
-            id = int(tokens[0])
-            if id == 1 and len(sentence) > 0:
-                dataset.append(sentence)
-                del sentence[:]
-            token = {
-                "id": id,
-                "orth": tokens[1],
-                "tag": tokens[3],
-                # "ner":
-                "head": id - head,
-                "dep": tokens[6],
+    sentences = []
+    words = []
+    docs = []
+    for i,item in enumerate(dataset):
+        bad = False
+        for token in item:
+            words.append(token["orth"])
+            h = token["head"] + token["id"]
+            #print(h,len(item))
+            if h < 0 or  h >= len(item):
+                print(item)
+                bad = True
+                break
+        if bad:
+            continue
+        sentences.append({"tokens":item})
+        if len(sentences) > 4:
+            doc = {
+                "id": i,
+                "paragraphs":[{
+                    "raw": " ".join(words),
+                    "sentences": list(sentences)
+                }]
             }
-            sentence.append(token)
-        trainset = []
-        testset = []
-        for i, item in enumerate(dataset):
-            if i % 10 == 0:
-                testset.append(item)
-            else:
-                trainset.append(item)
+            docs.append(doc)
+            del words[:]
+            del sentences[:]
 
-process_data(sys.argv[1])
+    if len(docs)> 0 and len(sentences)>0: 
+        doc = {
+            "id": docs[-1]["id"] + 1,
+            "paragraphs":[{
+                "raw": " ".join(words),
+                "sentences": list(sentences)
+            }]
+        }
+        docs.append(doc)
+    with open(filename,"w") as f:
+        json.dump(docs,f)
+
+
+def process_data(trainname,testname):
+    dataset = []
+    sentence = []
+    for l in sys.stdin:
+        tokens = l.split()
+        #print(tokens)
+        if len(tokens)  < 2:
+            if len(sentence) > 0:
+                dataset.append(list(sentence))
+                del sentence[:]
+            continue
+        head = int(tokens[6])
+        id = int(tokens[0]) -1
+        print(head,id)
+        h = head - id
+        #print(h)
+        token = {
+            "id": id,
+            "orth": tokens[1],
+            "tag": tokens[3],
+            # "ner":
+            "head": h,
+            "dep": tokens[7],
+        }
+        sentence.append(token)
+    trainset = []
+    testset = []
+    for i, item in enumerate(dataset):
+        if i % 10 == 0:
+            testset.append(item)
+        else:
+            trainset.append(item)
+    save_data(trainname,trainset)
+    save_data(testname,testset)
+
+process_data(sys.argv[1],sys.argv[2])
