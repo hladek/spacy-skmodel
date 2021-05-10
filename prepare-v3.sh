9495794eb08f3976043a958302e5dd39e3204995
Daniel Hladek <hladek@quadro.kemt.fei.tuke.sk>
2021-05-10 15:38:26 +0200

    zz

--- prepare-v3.sh
+++ prepare-v3.sh
@@ -1,4 +1,5 @@
-conda install -c conda-forge -c pytorch spacy cupy pytorch cudatoolkit=9.2
+conda install -c pytorch cupy pytorch cudatoolkit=9.2
+conda install -c conda-forge spacy
 # packages only available via pip
 pip install spacy-transformers spacy-lookups-data
 spacy init config config.cfg -l sk -p tagger,parser
