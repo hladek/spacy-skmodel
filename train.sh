f567467f7d6304c6a1902bcedb5f3f75094cebef
Daniel Hladek <hladek@quadro.kemt.fei.tuke.sk>
2021-12-02 15:33:43 +0100

    train

--- train.sh
+++ train.sh
@@ -1,14 +1,15 @@
 set -e # fail on error
-CUDA_VISIBLE_DEVICES=3 spacy train config.cfg -o ./train/posparser
-# Train POS and dependencies
+# cleanup old results
 rm -r dist
 mkdir -p dist
+#CUDA_VISIBLE_DEVICES=3 spacy train config.cfg -o ./train/posparser -g 0
+# Train POS and dependencies
 mkdir dist/posparser
 spacy package train/posparser/model-best dist/posparser
 # install to include pos and dependencies in new model
 pip install --force dist/posparser/sk_pipeline-0.0.0/dist/sk_pipeline-0.0.0.tar.gz
 # Train NER, copy POS and dep from old model
-CUDA_VISIBLE_DEVICES=3 spacy train config-ner.cfg -o ./train/nerposparser
+CUDA_VISIBLE_DEVICES=3 spacy train config-ner.cfg -o ./train/nerposparser -g 0
 # Correct meta
 cp ./train/nerposparser/model-best/meta.json ./train/nerposparser/model-best/meta-ner.json
 python changemeta.py ./train/posparser/model-best ./train/nerposparser/model-best
